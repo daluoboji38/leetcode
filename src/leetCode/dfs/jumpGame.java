accb50aed3b8dad032e0bb7b4c39d8c1cd9d4d06
daluoboji38 <daluoboji22@gmail.com>
2026-05-17 08:30:50 +0800

    260517

---
+++ src/leetCode/dfs/jumpGame.java
@@ -0,0 +1,21 @@
+package leetCode.dfs;
+
+// 跳跃游戏 III
+public class jumpGame {
+    public boolean canReach(int[] arr, int start) {
+        int m=arr.length;
+        boolean[] dp = new boolean[m];
+        return dfs(arr, start, dp);
+    }
+
+    private boolean dfs(int[] arr, int start, boolean[] dp) {
+        if (start < 0 || start >= arr.length || dp[start]) {
+            return false;
+        }
+        if(arr[start]==0){
+            return true;
+        }
+        dp[start]=true;
+        return dfs(arr, start-arr[start], dp) || dfs(arr, start+arr[start], dp);
+    }
+}
