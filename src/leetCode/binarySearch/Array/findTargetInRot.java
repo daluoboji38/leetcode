3fd079c3b05dbe20dfceb3841eb58aff5236cc59
0027006003@iwalecloud.com <daluoboji22@gmail.com>
2026-05-22 01:28:11 +0800

    260522

---
+++ src/leetCode/binarySearch/Array/findTargetInRot.java
@@ -0,0 +1,28 @@
+package leetCode.binarySearch.Array;
+
+// 搜索旋转排序数组
+public class findTargetInRot {
+    public int search(int[] nums, int target) {
+        if(nums==null || nums.length==0) return -1;
+        int left = 0,right = nums.length-1;
+        int mid;
+        while(left<=right){
+            mid = (left+right)>>>1;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target<nums[mid]){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }else{
+                if(nums[mid]<target && target<=nums[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
