cb4c1de07bed12db99a1e6023c64c9a679390428
0027006003@iwalecloud.com <daluoboji22@gmail.com>
2026-05-29 17:59:45 +0800

    260529-2

--- src/leetCode/Main.java
+++ src/leetCode/Main.java
@@ -1,6 +1,7 @@
 package leetCode;
 
 import leetCode.backtrack.Array.premutation;
+import leetCode.backtrack.Array.premutationDup;
 import leetCode.general.String.specialLetter2;
 import leetCode.greedy.Array.jumpGame4;
 import leetCode.greedy.String.regexMatch2;
@@ -10,8 +11,8 @@ import java.util.List;
 // % 是求余运算符，而 / 才是求商运算符
 public class Main {
     public void main(String[] args) {
-        premutation ex = new premutation();
-        List<List<Integer>> result = ex.permute(new int[]{1,2,3});
+        premutationDup ex = new premutationDup();
+        List<List<Integer>> result = ex.permuteUnique(new int[]{1,1,2});
         System.out.println(result);
 
 
--- src/leetCode/backtrack/Array/premutationDup.java
+++ src/leetCode/backtrack/Array/premutationDup.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// 全排列
-public class premutation {
-    public List<List<Integer>> permute(int[] nums) {
+// 全排列II
+public class premutationDup {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         int n = nums.length;
+        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> path = Arrays.asList(new Integer[n]);
         boolean[] visited = new boolean[n];
@@ -23,6 +24,7 @@ public class premutation {
         }
 
         for(int j = 0; j < nums.length; j++) {
+            if(j > 0 && nums[j] == nums[j - 1] && !visited[j - 1]) continue;
             if(!visited[j]) {
                 path.set(i, nums[j]);
                 visited[j] = true;
